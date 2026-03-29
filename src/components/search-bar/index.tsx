fe8e4d40c8fddd9a9960d9a12aa3e42c17c62dbf
caioWd <caio.wdl@gmail.com>
2026-03-29 17:55:05 -0300

    feat: add search bar

---
+++ src/components/search-bar/index.tsx
@@ -0,0 +1,27 @@
+import { StyledSearchBar } from "./styles"
+import type { SearchbarProps } from "react-native-paper";
+
+export interface StyledSearchBarProps extends SearchbarProps {
+  value: string
+  placeholder?: string
+  hasClearIcon?: boolean
+  onChangeValue: (value: string) => void
+}
+const SearchBar = ({
+  value,
+  placeholder,
+  hasClearIcon,
+  onChangeValue,
+}: StyledSearchBarProps) => {
+
+  return (
+    <StyledSearchBar
+      value={value}
+      placeholder={placeholder}
+      clearIcon={hasClearIcon ? 'close' : undefined}
+      onChangeText={onChangeValue}
+    />
+  )
+}
+
+export default SearchBar
---
+++ src/components/search-bar/styles.ts
@@ -0,0 +1,13 @@
+import { Searchbar } from "react-native-paper";
+import { styled } from "styled-components/native";
+
+export const StyledSearchBar = styled(Searchbar).attrs(() => ({
+  iconColor: '#414141',
+  placeholderTextColor: '#414141',
+  selectionColor: '#1E1E1E', 
+  inputStyle: {
+    color: '#1E1E1E',
+  },
+}))`
+  background-color: #ffffff;
+`;
